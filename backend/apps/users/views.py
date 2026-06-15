5fe382da87aa07bb552f9ce6f4171bd9dac7f565
Angeuh <lucas.larrey@gmail.com>
2026-06-15 19:04:27 +0200

    feature: working for jaeone

--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -1,11 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from .serializers import UserSerializer
+from .serializers import UserSerializer, ReviewSerializer
 
 
 class UserView(APIView):
-    Permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
     def get(self, request):
         serializer = UserSerializer(request.user)
@@ -21,6 +21,15 @@ class UserView(APIView):
             serializer.save()
             return Response({"success": True, "user": serializer.data})
         return Response(serializer.errors, status=400)
+
+
+class AvatarUpdateView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def patch(self, request):
+        request.user.avatar_url = request.data.get("avatar_url")
+        request.user.save()
+        return Response({"avatar_url": request.user.avatar_url})
 
 
 def test_error(request):
@@ -46,3 +55,23 @@ class OnboardingView(APIView):
             })
 
         return Response(serializer.errors, status=400)
+
+
+class UserActivityView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get (self, request):
+        user = request.user
+        interactions = user.interactions.select_related("media").all()
+        reviews = user.review.select_related("media").all()
+
+        data = {
+            "interactions": {
+                "like": [],
+                "dislike": [],
+                "watchlist": [],
+                "watched": [],
+            }
+            "reviews": ReviewSerializer(reviews, many=True).data
+        }
+
